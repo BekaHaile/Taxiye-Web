f92c880af963494a028a28ea04c6ef0781709350
kalkidanbehayilu <kalkidanbehayilu@gmail.com>
2021-09-18 21:29:07 +0300

    added title to the page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -95,6 +95,7 @@ function withHeader(loading, pageProps, Component) {
       <Provider store={store}>
         <QueryClientProvider client={queryClient}>
           <Head>
+            <title>Taxiye Always Moving!</title>
             <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link
               href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap"
@@ -147,6 +148,7 @@ function withOutHeader(loading, pageProps, Component) {
     <>
       <Provider store={store}>
         <Head>
+          <title>Taxiye Always Moving!</title>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap"
